274c571b71a9f1e687d058133158b9b8af9277b1
Zouheir Mohamed <m.zouheir74@gmail.com>
2023-11-25 14:04:44 +0100

    add user fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,16 +3,35 @@
 namespace App\DataFixtures;
 
 use App\Entity\Product;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {}
+
     public function load(ObjectManager $manager): void
     {
-        for ($i = 0; $i < 16; $i++) { 
+        for ($i = 0; $i < 16; $i++) {
+            $user = new User();
+            $user->setEmail('email' . $i . '@example.com');
+            $user->setFirstname('firstname' . $i);
+            $user->setLastname('lastname' . $i);
+            $user->setUsername('username'.$i);
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $user,
+                'user'.$i
+            );
+            $user->setPassword($hashedPassword);
+            $user->setRoles([User::ROLE_USER]);
+            $manager->persist($user);
+
+
             $product = new Product();
-            $product->setName('Product'. $i);
+            $product->setName('Product' . $i);
             $product->setPrice(rand(1, 9));
             $product->setDescription('This is a product description !');
 
