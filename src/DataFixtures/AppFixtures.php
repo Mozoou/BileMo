517005869ef4e2024813ef3657dd9c89b2f75b85
Zouheir Mohamed <m.zouheir74@gmail.com>
2023-12-07 10:29:10 +0100

    update fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,7 +2,9 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Product;
+use App\Entity\Brand;
+use App\Entity\Client;
+use App\Entity\MobilePhone;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -16,26 +18,40 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         for ($i = 0; $i < 16; $i++) {
-            $user = new User();
-            $user->setEmail('email' . $i . '@example.com');
-            $user->setFirstname('firstname' . $i);
-            $user->setLastname('lastname' . $i);
-            $user->setUsername('username'.$i);
+            $company = new User();
+            $company->setEmail('company' . $i . '@example.com');
             $hashedPassword = $this->passwordHasher->hashPassword(
-                $user,
-                'user'.$i
+                $company,
+                'company'.$i
             );
-            $user->setPassword($hashedPassword);
-            $user->setRoles([User::ROLE_USER]);
-            $manager->persist($user);
+            $company->setPassword($hashedPassword);
+            $company->setRoles([User::ROLE_USER]);
 
+            for ($j = 0; $j < 3; $j++) { 
+                $client = new Client();
+                $client->setCompany($company);
+                $client->setFirstName('ClientFirstName_'. $j);
+                $client->setLastName('ClientLastName_'. $j);
+                $client->setUsername('ClientUsername'.$i);
+                $client->setEmail('client'. $j . '@example.com');
 
-            $product = new Product();
-            $product->setName('Product' . $i);
-            $product->setPrice(rand(1, 9));
-            $product->setDescription('This is a product description !');
+                $manager->persist($client);
+            }
 
-            $manager->persist($product);
+            $manager->persist($company);
+
+            $brand = new Brand();
+            $brand->setName('PhoneBrand');
+            $brand->setCountryCode('FR');
+            $manager->persist($brand);
+
+            $mobilePhone = new MobilePhone();
+            $mobilePhone->setBrand($brand);
+            $mobilePhone->setName('Phone_' . $i);
+            $mobilePhone->setPrice(rand(200, 900));
+            $mobilePhone->setDescription('This is a mobile phone description !');
+
+            $manager->persist($mobilePhone);
         }
 
 
